48d3f2a65c012b0a771448ca9dbf4f6eb12be661
FKRF <fabiankung@users.noreply.github.com>
2021-02-09 15:11:10 +0800

    Added codes for Example 2

---
+++ MVM_Arduino_Example/Example2/Example2.ino
@@ -0,0 +1,35 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(57600);
+  delay(500);           // A 500 ms delay for the module to initialize properly.
+  Serial.write(0x10);   // Run IPA (image processing algorithm) 1 on interval 1.
+  Serial.write(0x20);   // Run IPA 2 on interval 2.
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+int nID, nLuminance, nX, nY;
+int nRow0, nRow1, nRow2;
+
+  if (Serial.available() > 3) // Make sure at least 4 bytes in the received buffer.
+  {
+    nID = Serial.read();      // Get process ID.
+    if (nID == 1)             // Make sure the data is from IPA 1.
+    {
+      nLuminance = Serial.read();
+      nX = Serial.read();
+      nY = Serial.read();
+    }
+    else if (nID == 2)       // Make sure the data is from IPA 2.
+    {
+      nRow2 = Serial.read();
+      nRow1 = Serial.read();
+      nRow0 = Serial.read();
+    }
+    else
+    {
+      Serial.flush();     // Flush received buffer.
+    }
+  }
+}
